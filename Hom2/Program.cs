5bfccdea4a4a7eebf8bc6fd893e9cc61d26d137e
Денис <den.wtrui2017@gmail.com>
2023-03-17 10:53:24 +0100

    no message

---
+++ Hom2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hom2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
